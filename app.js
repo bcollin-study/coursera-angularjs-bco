41ce339d7867441d2c4868f67235c0df59758bde
Branko Collin Study <bcollin-study@abeleto.nl>
2023-06-16 12:20:38 +0200

    Lecture 12: filters.

--- app.js
+++ app.js
@@ -8,6 +8,9 @@
 		$scope.name = '';
 		$scope.light_state = 'off';
 		$scope.state_other = 'on';
+		var costPerChar = .0085; // price of a character in USD.
+		$scope.cost = 0;
+		
 		
 		$scope.sum = function () {
 			var totalNameValue = 0;
@@ -15,12 +18,14 @@
 			for (var i = 0; i < $scope.name.length; i++) {
 				totalNameValue += $scope.name.charCodeAt(i);
 			}
+			
+			$scope.cost = totalNameValue * costPerChar;
 			return totalNameValue;
 		}
 		
 		$scope.upper = function() {
-			var upCase = $filter('uppercase');
-			$scope.name = upCase($scope.name);
+			var output = $filter('uppercase')($scope.name);
+			$scope.name = output;
 		}
 		
 		$scope.lightToggle = function() {
