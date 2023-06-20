08e339227ca9fde36a4407238f79982b8d6c0515
Branko Collin Study <bcollin-study@abeleto.nl>
2023-06-20 13:27:43 +0200

    Added the ability to add items.

--- shopping-list.js
+++ shopping-list.js
@@ -4,6 +4,7 @@
 	var shoppingList1 = ['apple', 'pear', 'strawberry'];
 	
 	var shoppingList2 = [
+			{name: 'giant table', qty: '1'},
 			{name: 'spoon', qty: '50'},
 			{name: 'fork',  qty: '30'},
 			{name: 'knife', qty: '70'},
@@ -17,6 +18,18 @@
 	function shoppingListController($scope) {
 		$scope.shoppingList1 = shoppingList1;
 		$scope.shoppingList2 = shoppingList2;
+		$scope.newItem = '';
+		
+		$scope.addItem = function(){
+			if ( $scope.newItem !== '') {
+				var count = $scope.shoppingList2.length;
+				$scope.shoppingList2[count] = { name: $scope.newItem, qty: 50 };
+			}
+		}
+		
+		$scope.multiplier = function(val) {
+			if (val > 1) { return 's'; }
+		}
 	}
 	
 	
