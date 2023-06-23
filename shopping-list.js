45d6f5d74c666577410c3ca63823a10ed43b001f
Branko Collin Study <bcollin-study@abeleto.nl>
2023-06-24 01:58:26 +0200

    Enable multiple shopping lists by using services from a factory.

--- shopping-list.js
+++ shopping-list.js
@@ -22,7 +22,7 @@
 		return value.indexOf(searchValue) !== -1; 
 	}
 	
-	function shoppingListService() {
+	function shoppingListService(maxItems) {
 		var service = this;
 		var items = [];
 		
@@ -31,11 +31,21 @@
 		};
 		
 		service.addItem = function(item, qty) {
-			var item = {
-				name: item,
-				qty: qty
-			};
-			items.push(item);
+			console.log('Max items: ', maxItems);
+			if (maxItems === undefined || items.lenght < maxItems) {
+				var item = {
+					name: item,
+					qty: qty
+				};
+				
+				console.log(items);
+				items.push(item);
+				console.log(items);
+			}
+			else {
+				console.log('number of items exceeds limit');
+				throw new Error("Max items (" + maxItems + ") reached, stop adding items!");
+			}
 		};
 		
 		service.removeItem = function(idx) {
@@ -52,85 +62,78 @@
 		
 		service.setItem = function(idx, val) {
 			items[idx] = val;
-			console.log(items);
 		}
 	}
 	
-	console.log(numberList.filter(removeSmallerThanFive));
-	
-	console.log('Filtered for ' + searchValue + ', product list = ', shoppingList1.filter(containsFilter));
-
 	angular.module('shoppingList', [])
-	.controller('shoppingListController', shoppingListController)
-	.controller('shoppingListAddController', shoppingListAddController)
-	.controller('parentController', parentController)
-	.controller('child1Controller', child1Controller)
-	.controller('child2Controller', child2Controller)
-	.service('shoppingListService', shoppingListService);
+	.controller('shoppingList1Controller', shoppingList1Controller)
+	.controller('shoppingList2Controller', shoppingList2Controller)
+	.factory('shoppingListFactory', shoppingListFactory);
 	
-	shoppingListController.$inject = ['shoppingListService'];
-	function shoppingListController(shoppingListService) {
-		var show = this;
+	shoppingList1Controller.$inject = ['shoppingListFactory'];
+	function shoppingList1Controller(shoppingListFactory) {
+		var ctrl = this;
+		var service = shoppingListFactory(3, 'list1');
 		
-		show.shoppingList1 = shoppingList1;
-		shoppingListService.init();
-		show.shoppingList2 = shoppingListService.getItems();
-		console.log('Initial shopping list: ', show.shoppingList2);
+		service.init();
+		ctrl.shoppingList = service.getItems();
 		
-		show.multiplier = function(val) {
+		ctrl.multiplier = function(val) {
 			if (val > 1) { return 's'; }
 		};
 		
-		show.removeItem = function(idx) {
-			shoppingListService.removeItem(idx); 
+		ctrl.removeItem = function(idx) {
+			service.removeItem(idx); 
 		};
+
+		ctrl.newItem = '';
+		ctrl.newQty = '';
+		
+		ctrl.addItem = function(){
+			if ( ctrl.newItem !== '') {
+				if (ctrl.newQty === '') { ctrl.newQty = '1'; }
+				try {
+					service.addItem(ctrl.newItem, ctrl.newQty);
+				} catch (error) {
+					ctrl.errorMessage = error.message;
+				}
+			}
+		}
 	}
 	
-	shoppingListAddController.$inject = ['shoppingListService'];
-	function shoppingListAddController(shoppingListService) {
-		console.log('Service: ', shoppingListService);
-		var ia = this;
+	shoppingList2Controller.$inject = ['shoppingListFactory'];
+	function shoppingList2Controller(shoppingListFactory) {
+		var ctrl2 = this;
+		var service = shoppingListFactory();
 		
-		ia.newItem = '';
-		ia.newQty = '';
+		service.init();
+		ctrl2.shoppingList = service.getItems();
 		
-		ia.firstQty = shoppingListService.getItem(0).qty;
+		ctrl2.multiplier = function(val) {
+			if (val > 1) { return 's'; }
+		};
 		
-		ia.addItem = function(){
-			if ( ia.newItem !== '') {
-				if (ia.newQty === '') { ia.newQty = '1'; }
-				shoppingListService.addItem(ia.newItem, ia.newQty);
+		ctrl2.removeItem = function(idx) {
+			service.removeItem(idx); 
+		};
+		
+		ctrl2.newItem = '';
+		ctrl2.newQty = '';
+		
+		ctrl2.addItem = function(){
+			if ( ctrl2.newItem !== '') {
+				if (ctrl2.newQty === '') { ctrl2.newQty = '1'; }
+				service.addItem(ctrl2.newItem, ctrl2.newQty);
 			}
 		}
 		
-		ia.setFirstQty = function() {
-			item = shoppingListService.getItem(0);
-			item.qty = ia.firstQty;
-			shoppingListService.setItem(0, item);
-		}
+	}
+	
+	function shoppingListFactory() {
+		var factory = function(maxItems) {
+			return new shoppingListService(maxItems);
+		};
 		
+		return factory;
 	}
-	
-	parentController.$inject = ['$scope'];
-	function parentController($scope) {
-		var parent = this;
-		$scope.parentValue = parent.parentValue = 5;
-		$scope.pc = this;
-		$scope.pc.parentValue = 3;
-	}
-	
-	child1Controller.$inject = ['$scope'];
-	function child1Controller($scope) {
-		var child1 = this;
-		console.log('$scope.parentValue: ', $scope.parentValue);
-		console.log('CHILD $scope', $scope);
-		$scope.pc.parentValue = 20;
-		$scope.parentValue = child1.parentValue = 30;
-		
-	}
-	
-	child2Controller.$inject = ['$scope'];
-	function child2Controller($scope) {
-	}
-	
 })()
