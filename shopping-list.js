418457f15bfde50498567b33cc81fcf3744dfeb3
Branko Collin Study <bcollin-study@abeleto.nl>
2023-06-26 09:51:13 +0200

    Factory set-up cleaned up to have a base.

--- shopping-list.js
+++ shopping-list.js
@@ -31,19 +31,15 @@
 		};
 		
 		service.addItem = function(item, qty) {
-			console.log('Max items: ', maxItems);
-			if (maxItems === undefined || items.lenght < maxItems) {
+			if (maxItems === undefined || items.length < maxItems) {
 				var item = {
 					name: item,
 					qty: qty
 				};
 				
-				console.log(items);
 				items.push(item);
-				console.log(items);
 			}
 			else {
-				console.log('number of items exceeds limit');
 				throw new Error("Max items (" + maxItems + ") reached, stop adding items!");
 			}
 		};
@@ -66,14 +62,13 @@
 	}
 	
 	angular.module('shoppingList', [])
-	.controller('shoppingList1Controller', shoppingList1Controller)
-	.controller('shoppingList2Controller', shoppingList2Controller)
+	.controller('shoppingListController', shoppingListController)
 	.factory('shoppingListFactory', shoppingListFactory);
 	
-	shoppingList1Controller.$inject = ['shoppingListFactory'];
-	function shoppingList1Controller(shoppingListFactory) {
+	shoppingListController.$inject = ['shoppingListFactory'];
+	function shoppingListController(shoppingListFactory) {
 		var ctrl = this;
-		var service = shoppingListFactory(3, 'list1');
+		var service = shoppingListFactory(undefined, 'list1');
 		
 		service.init();
 		ctrl.shoppingList = service.getItems();
@@ -99,34 +94,6 @@
 				}
 			}
 		}
-	}
-	
-	shoppingList2Controller.$inject = ['shoppingListFactory'];
-	function shoppingList2Controller(shoppingListFactory) {
-		var ctrl2 = this;
-		var service = shoppingListFactory();
-		
-		service.init();
-		ctrl2.shoppingList = service.getItems();
-		
-		ctrl2.multiplier = function(val) {
-			if (val > 1) { return 's'; }
-		};
-		
-		ctrl2.removeItem = function(idx) {
-			service.removeItem(idx); 
-		};
-		
-		ctrl2.newItem = '';
-		ctrl2.newQty = '';
-		
-		ctrl2.addItem = function(){
-			if ( ctrl2.newItem !== '') {
-				if (ctrl2.newQty === '') { ctrl2.newQty = '1'; }
-				service.addItem(ctrl2.newItem, ctrl2.newQty);
-			}
-		}
-		
 	}
 	
 	function shoppingListFactory() {
