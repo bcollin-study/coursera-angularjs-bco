76511722200b31a85bf45269ca8e5de3a1129b75
Branko Collin Study <bcollin-study@abeleto.nl>
2023-07-20 16:41:16 +0200

    Two types of directives, one using 'template', the other 'templateUrl'.

---
+++ news.js
@@ -0,0 +1,69 @@
+(function(){
+	'use strict';
+
+	angular.module('newsApp', [])
+	.controller('newsController', newsController)
+	.service('newsService', newsService)
+	.directive('newsItem', newsItemDirective)
+	.directive('comments', commentsDirective);
+	
+	function commentsDirective() {
+		var ddo = {
+			templateUrl: 'comments-template.html'
+		};
+		
+		return ddo;
+	};
+	
+	function newsItemDirective() {
+		var ddo = {
+			template: `
+				<h3>{{item.title}}</h3> 
+				
+				<div class=\"story\">{{item.story}}</div>
+			`
+		};
+		return ddo;
+	};
+	
+	newsController.$inject = ['newsService'];
+	function newsController(newsService){
+		var ct = this;
+		ct.news = newsService.news;
+		
+		ct.newComment = [];
+		
+		ct.addComment = function(idx) {
+			newsService.addComment(idx, ct.newComment[idx]);
+			ct.newComment[idx] = '';
+		}
+	};
+	
+	function newsService(){
+		var sv = this;
+		var startidx = 0;
+		
+		sv.news = {
+			items: [
+				{
+					id: startidx++,
+					title: 'item 1',
+					story: 'story 1',
+					comments: [{text: 'cool story, bro', date: '2023-05-01'},{text: 'turgid tale, comrade', date: '2023-07-03'}]
+				},
+				{
+					id: startidx++,
+					title: 'item 2',
+					story: 'story 2',
+					comments: [{text: 'sus story, sis', date: '2023-06-02'}]
+				}
+			],
+		};
+		
+		sv.addComment = function(idx, comment) {
+			var commentObj = {text: comment, date: Date()};
+			sv.news.items[idx].comments.push(commentObj);
+		};
+	};
+
+})();
